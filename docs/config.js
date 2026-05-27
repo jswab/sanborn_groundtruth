c0f775cdb365a12eb9a9376a7b4df674dfa438ce
Jack Swab <104881718+jswab@users.noreply.github.com>
2026-05-27 13:54:42 -0400

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -4,6 +4,6 @@ const CONFIG = {
     { name: "Mia",  password: "redbull" },
   ],
 
-  googleSheetWebAppUrl: "",
-  googleSheetSecret: "",
+  googleSheetWebAppUrl: "https://script.google.com/macros/s/AKfycbx4lNF8G-KkjR-mzwxUSySiRzfZCJaRHjM8c7isTg0UMizr6uOMYsJW4SlPUXXmgdXxhA/exec",
+  googleSheetSecret: "sanborn-gt-2026-xk9mq",
 };
