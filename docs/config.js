cd1daa2e93733db7661d26ebe98a89a23b699034
Jack Swab <104881718+jswab@users.noreply.github.com>
2026-05-29 13:24:16 -0400

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
     { name: "Jack",            password: "sanborn1" },
     { name: "Mia",  password: "redbull" },
     { name: "Ian",  password: "hotslop" },
+    { name: "Tim",  password: "GeoSus" },
   ],
 
   googleSheetWebAppUrl: "https://script.google.com/macros/s/AKfycbx4lNF8G-KkjR-mzwxUSySiRzfZCJaRHjM8c7isTg0UMizr6uOMYsJW4SlPUXXmgdXxhA/exec",
